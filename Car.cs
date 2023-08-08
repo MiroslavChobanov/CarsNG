f09f9b482cc66202cce709b58fa8dcc22dcb2a9f
MiroslavChobanov <81228336+MiroslavChobanov@users.noreply.github.com>
2023-08-08 23:08:28 +0300

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,10 @@
+﻿namespace CarsAPI
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string PlateNumber { get; set; } = string.Empty;
+    }
+}
